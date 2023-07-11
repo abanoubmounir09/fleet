c639f376f9fef252364d33a3166d32396b77c5fb
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-07-11 10:47:23 +0300

    edit

--- fleet/fleet_managment/setup.py
+++ fleet/fleet_managment/setup.py
@@ -1,6 +1,8 @@
 
 
 import frappe
+from frappe import _
+
 DOMAINS = frappe.get_active_domains()
 
 def install() :
@@ -30,8 +32,8 @@ def hook_on_submit(doc,*args, **kwargs):
         kwargs['owner_name'] = doc.applicant_name
         kwargs['document_type'] = doc.doctype
         kwargs['document_name'] = doc.name
-        kwargs['subject'] = "Vechile Log Is Submitted For Driver {0}  at {1} status become Completed".format(doc.applicant_name, doc.modified)
-        kwargs['email_content'] = "Vechile Log Is Submitted For Driver {0}  at {1} status become Completed".format(doc.applicant_name, doc.modified)
+        kwargs['subject'] = _("Vechile Log Is Submitted For Driver {0} at {1} status become Completed").format(doc.applicant_name, doc.modified)
+        kwargs['email_content'] = _("Vechile Log Is Submitted For Driver {0} at {1} status become Completed").format(doc.applicant_name, doc.modified)
         send_alert_vechile_driver(**kwargs)
         
 
