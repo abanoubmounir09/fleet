f09818c50008f2618b198b56c58c4fe007808650
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-07-11 09:39:11 +0300

    edit

--- fleet/fleet_managment/setup.py
+++ fleet/fleet_managment/setup.py
@@ -7,7 +7,6 @@ def install() :
     pass
 
 def create_vehicle_log_script(doc,*args, **kwargs):
-    print("hello world from hook")
     odoreading=doc.odometer or 0
     result=frappe.db.sql("""select type from `tabVehicle Service` where parent='%s' group by type"""%doc.name,as_dict=1)
     card_name = frappe.db.sql("""select name from `tabVehicle Card` where vehicle='%s'""" % doc.license_plate, as_dict=1)
@@ -16,7 +15,7 @@ def create_vehicle_log_script(doc,*args, **kwargs):
         card_doc = frappe.get_doc("Vehicle Card", card_name[0].name)
         for s in result:
             for d in card_doc.maintainance:
-                if s.type==d.maintainance and odoreading >d.odometer_reading:
+                if s.type==d.maintainance and odoreading > d.odometer_reading:
                     print("from last if")
                     d.odometer_reading=odoreading
         card_doc.save()
