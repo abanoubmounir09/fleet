c84ac98b532dba6ea82fd313bb11611954b1e4e1
Hagar Mossad <hagermossad80@gmail.com>
2023-10-24 14:31:49 +0300

    modify naming

--- fleet/controllers/vehicle_log.py
+++ fleet/controllers/vehicle_log.py
@@ -2,14 +2,14 @@ import frappe
 
 @frappe.whitelist()
 def create_journal_entry(source_name):
-    doc = frappe.get_doc("Vehicle Log",source_name)
+    vehicle_log = frappe.get_doc("Vehicle Log",source_name)
     journal_entry = frappe.new_doc("Journal Entry")
-    journal_entry.vehicle_log = doc.name
+    journal_entry.vehicle_log = vehicle_log.name
     return journal_entry
 
 @frappe.whitelist()
 def create_purchase_invoice(source_name):
-    doc = frappe.get_doc("Vehicle Log",source_name)
-    journal_entry = frappe.new_doc("Purchase Invoice")
-    journal_entry.vehicle_log = doc.name
-    return journal_entry
+    vehicle_log = frappe.get_doc("Vehicle Log",source_name)
+    purchase_invoice = frappe.new_doc("Purchase Invoice")
+    purchase_invoice.vehicle_log = vehicle_log.name
+    return purchase_invoice
